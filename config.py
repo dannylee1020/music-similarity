2509673bcfcde3a6d7bc794803048350f2956d0e
dannylee1020 <dannylee1020@gmail.com>
2020-09-17 12:09:50 -0700

    adding some changes

--- config.py
+++ config.py
@@ -3,5 +3,10 @@ BASE_DIR = '/Users/dhyungseoklee/Projects/ML_Pipelines/music-simliarity/file/'
 LR_TEST = 'lr_test.npy'
 SIM_TEST = 'sim_test.npy'
 Y_TEST = 'y_test.npy'
+EMBEDDING_MATRIX = 'word_embedding_matrix.npy'
+NB_WORDS = 'nb_words.json'
 
 MODEL = 'lstm_model.h5'
+MAX_SEQ_LEN = 100
+EMBEDDING_DIM = 300
+DROPOUT = 0.2
